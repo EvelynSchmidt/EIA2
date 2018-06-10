7dcc1481b8ee22ff93f2dbf785d8317fdec845b3
evelynschmidt <evelynschmidt@192.168.0.23>
2018-06-10 15:55:50 +0200

    push

---
+++ Aufgabe9/ProcessForm.js
@@ -0,0 +1,70 @@
+var Aufgabe8;
+(function (Aufgabe8) {
+    window.addEventListener("load", init);
+    var address = "https://projekteia2.herokuapp.com";
+    var inputs = document.getElementsByTagName("input");
+    function init(_event) {
+        console.log("Init");
+        var insertButton = document.getElementById("insert");
+        var refreshButton = document.getElementById("refresh");
+        var searchButton = document.getElementById("checkSearch");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        searchButton.addEventListener("click", search);
+    }
+    function insert(_event) {
+        var genderButton = document.getElementById("male");
+        var matrikel = inputs[2].value;
+        var studi;
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt(matrikel),
+            age: parseInt(inputs[3].value),
+            gender: genderButton.checked,
+            studiengang: document.getElementsByTagName("select").item(0).value
+        };
+        var convert = JSON.stringify(studi);
+        console.log(convert);
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=insert&data=" + convert, true);
+        xhr.addEventListener("readystatechange", handleStateChangeInsert);
+        xhr.send();
+    }
+    function handleStateChangeInsert(_event) {
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function refresh(_event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=refresh", true);
+        xhr.addEventListener("readystatechange", handleStateChangeRefresh);
+        xhr.send();
+    }
+    function handleStateChangeRefresh(_event) {
+        var output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+    function search(_event) {
+        var mtrkl = inputs[6].value;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=search&searchFor=" + mtrkl, true);
+        xhr.addEventListener("readystatechange", handleStateChangeSearch);
+        xhr.send();
+    }
+    function handleStateChangeSearch(_event) {
+        var output = document.getElementsByTagName("textarea")[1];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+})(Aufgabe8 || (Aufgabe8 = {}));
+//# sourceMappingURL=ProcessForm.js.map
